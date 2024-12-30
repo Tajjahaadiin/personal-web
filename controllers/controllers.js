d8ae15449073dc00772d256b6856a755821c9233
Tajjuddiin <tajjuddiinauliya@gmail.com>
2024-12-31 02:48:34 +0700

    CRUD

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,16 +1,129 @@
+const { formatDateToWIB } = require("../utils/time");
+const { Sequelize, QueryTypes } = require("sequelize");
+const config = require("../config/config.json");
+
+const sequelize = new Sequelize(config.development);
 function renderHome(req, res) {
   res.render("index");
 }
-function renderProject(req, res) {
-  res.render("project");
+async function renderProject(req, res) {
+  const query = `SELECT * FROM public."Projects" ORDER By "createdAt" DESC`;
+  const projects = await sequelize.query(query, { type: QueryTypes.SELECT });
+  res.render("project", { projects: projects });
 }
-function addProject(req, res) {}
+function renderaddProject(req, res) {
+  res.render("addproject");
+}
+async function addBlog(req, res) {
+  const { inputTitle, inputContent, image, dateStart, dateEnd } = req.body;
+  const { angular, nodeJs, react, vueJs } = req.body;
+
+  let checkBox = [];
+  if (angular) {
+    checkBox.push(angular);
+  }
+  if (nodeJs) {
+    checkBox.push(nodeJs);
+  }
+  if (react) {
+    checkBox.push(react);
+  }
+  if (vueJs) {
+    checkBox.push(vueJs);
+  }
+
+  // const image = 'https://picsum.photos/200/300';
+
+  const query = `INSERT INTO public."Projects"
+                (title, content, image, teknologi)
+                VALUES
+                ('${inputTitle}', '${inputContent}', '${image}', '${checkBox}')
+  `;
+
+  const result = await sequelize.query(query, { type: QueryTypes.INSERT });
+
+  console.log(result);
+
+  res.redirect("/project");
+}
+async function renderProjectDetail(req, res) {
+  const { id } = req.params;
+  const query = `SELECT * FROM public."Blogs" WHERE id = ${id}`;
+  const project = await sequelize.query(query, { type: QueryTypes.SELECT });
+
+  res.render("project-detail", { data: project[0] });
+}
 function renderTestimonial(req, res) {
   res.render("testimonials");
 }
 function renderContact(req, res) {
   res.render("contact");
 }
+async function renderEditProject(req, res) {
+  const { id } = req.params;
+  const query = `SELECT * FROM public."Projects" WHERE id = ${id}`;
+  const project = await sequelize.query(query, { type: QueryTypes.SELECT });
+  const tech = project[0].teknologi;
+  console.log(tech);
+  const angular = tech.includes("Angular"); // true
+  const node = tech.includes("NodeJs");
+  const react = tech.includes("React"); //false
+  const vue = tech.includes("VueJs");
+
+  res.render("edit-project", {
+    data: project[0],
+    angular,
+    node,
+    react,
+    vue,
+  });
+}
+async function updateProject(req, res) {
+  const { id } = req.params;
+  const img = req.query.image;
+  let { inputTitle, inputContent, image } = req.body;
+  const { angular, nodeJs, react, vueJs } = req.body;
+
+  if (image == "") {
+    image = img;
+  }
+
+  let checkBox = [];
+  if (angular) {
+    checkBox.push(angular);
+  }
+  if (nodeJs) {
+    checkBox.push(nodeJs);
+  }
+  if (react) {
+    checkBox.push(react);
+  }
+  if (vueJs) {
+    checkBox.push(vueJs);
+  }
+
+  const query = `UPDATE public."Projects"
+                  SET title ='${inputTitle}', content ='${inputContent}', image ='${image}', teknologi ='${checkBox}'
+                 WHERE id = ${id}`;
+
+  const result = await sequelize.query(query, { type: QueryTypes.UPDATE });
+
+  console.log(result);
+
+  res.redirect("/project");
+}
+function deletProject(req, res) {
+  const { id } = req.params;
+
+  const query = `DELETE FROM public."Projects"
+                  WHERE id = ${id}`;
+  const result = sequelize.query(query, { type: QueryTypes.DELETE });
+
+  console.log("result query delete :", result);
+
+  res.redirect("/project");
+}
+
 function renderHandle(req, res) {
   res.render("404");
 }
@@ -20,4 +133,10 @@ module.exports = {
   renderTestimonial,
   renderContact,
   renderHandle,
+  renderaddProject,
+  addBlog,
+  renderProjectDetail,
+  renderEditProject,
+  updateProject,
+  deletProject,
 };
--- index.js
+++ index.js
@@ -7,14 +7,23 @@ const {
   renderTestimonial,
   renderContact,
   renderHandle,
+  renderaddProject,
+  addBlog,
+  renderProjectDetail,
+  renderEditProject,
+  updateProject,
+  deletProject,
 } = require("./controllers/controllers");
 
 const app = express();
 const port = 5000;
+const { formatDateToWIB, getRelativeTime } = require("./utils/time");
 
 // handlebars declaration
 app.set("view engine", "hbs");
 hbs.registerPartials(__dirname + "/views/partials", function (err) {});
+hbs.registerHelper("formatDateToWIB", formatDateToWIB);
+hbs.registerHelper("getRelativeTime", getRelativeTime);
 hbs.registerHelper("equal", function (a, b) {
   return a == b;
 });
@@ -26,12 +35,19 @@ app.use("/assets", express.static(path.join(__dirname, "./assets")));
 // middleware
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 // Rendering
 app.get("/", renderHome);
 app.get("/project", renderProject);
 app.get("/testimonial", renderTestimonial);
 app.get("/contact", renderContact);
+app.get("/addproject", renderaddProject);
+app.post("/addBlog", addBlog);
+app.get("/project-detail/:id", renderProjectDetail);
+app.get("/edit-project/:id", renderEditProject);
+app.patch("/update-project/:id", updateProject);
+app.delete("/delet-project/:id", deletProject);
 app.get("*", renderHandle);
 
 app.listen(port, () => {
